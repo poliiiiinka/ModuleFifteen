59f12eac8f11afbf9b6ef10ba3e3cd17ccbd265e
Полина Артамонова <poliiiiinka71@gmail.com>
2024-07-01 18:52:34 +0300

    Добавьте файлы проекта.

---
+++ SF_HW03_ModuleFifteen/Program.cs
@@ -0,0 +1,30 @@
+﻿namespace SF_HW03_ModuleFifteen
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // записываем учеников в классы
+            var classes = new[]
+            {
+               new Classroom { Students = {"Evgeniy", "Sergey", "Andrew"}, },
+               new Classroom { Students = {"Anna", "Viktor", "Vladimir"}, },
+               new Classroom { Students = {"Bulat", "Alex", "Galina"}, }
+           };
+            var allStudents = GetAllStudents(classes);
+
+            // задаём формат вывода
+            Console.WriteLine(string.Join(" ", allStudents));
+        }
+
+        static string[] GetAllStudents(Classroom[] classes)
+        {
+            return classes.SelectMany(c => c.Students).ToArray();
+        }
+
+        public class Classroom
+        {
+            public List<string> Students = new List<string>();
+        }
+    }
+}
